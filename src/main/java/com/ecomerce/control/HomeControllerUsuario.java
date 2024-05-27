e408e406e9b1ab6094e08248ef90072795a13766
JhonatanOropeza <jhon_oropeza810@hotmail.com>
2024-05-27 17:38:11 -0600

    Podemos visualizar los productos de manera individual e la vista de usuario

--- src/main/java/com/ecomerce/control/HomeControllerUsuario.java
+++ src/main/java/com/ecomerce/control/HomeControllerUsuario.java
@@ -1,16 +1,24 @@
 package com.ecomerce.control;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ecomerce.model.Producto;
 import com.ecomerce.service.ProductoService;
 
 @Controller
 @RequestMapping("/")
 public class HomeControllerUsuario {
+	
+	private final Logger log = LoggerFactory.getLogger(HomeControllerUsuario.class);
 	
 	@Autowired
 	private ProductoService productoService;
@@ -19,5 +27,16 @@ public class HomeControllerUsuario {
 	public String homeUsuario(Model model) {
 		model.addAttribute("productos", productoService.findAll());
 		return "usuario/homeUsuario";
+	}
+	
+	@GetMapping("productohome/{id}")
+	public String productoHomeUsuario(@PathVariable("id") Integer id, Model model) {
+		log.info("Id producto enviado como parámetro: {}", id);
+		Producto producto = new Producto();
+		Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get();
+		
+		model.addAttribute("producto", producto);
+		return "usuario/productohome";
 	}
 }
