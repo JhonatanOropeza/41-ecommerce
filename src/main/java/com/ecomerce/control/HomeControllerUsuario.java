77a6907c002438203c3e747e202bdb2ee34c7609
JhonatanOropeza <jhon_oropeza810@hotmail.com>
2024-05-28 19:25:17 -0600

    Se agrego la funcinalidad de búsqeuda

--- src/main/java/com/ecomerce/control/HomeControllerUsuario.java
+++ src/main/java/com/ecomerce/control/HomeControllerUsuario.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -160,5 +161,15 @@ public class HomeControllerUsuario {
 		detalles.clear();
 		
 		return "redirect:/";
+	}
+	
+	@PostMapping("/search")
+	public String searchProduct(@RequestParam("nombre") String nombre, Model model) {
+		log.info("Nombre del producto: {}", nombre);
+		List<Producto> productos = productoService.findAll().stream().filter( p -> p.getNombre().toLowerCase().toUpperCase().contains(nombre)).collect(Collectors.toList());
+		
+		model.addAttribute("productos", productos);
+		
+		return "usuario/homeUsuario";
 	}
 }
